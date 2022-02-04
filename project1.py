bc8e47ad19468eb0a4b7f4ecca6b85877f853989
Qifa Wang <qifwang@umich.edu>
2022-02-04 09:54:45 -0500

    Initial commit

---
+++ project1.py
@@ -0,0 +1,287 @@
+"""EECS 445 - Winter 2022.
+
+Project 1
+"""
+
+import pandas as pd
+import numpy as np
+import itertools
+import string
+
+from sklearn.svm import SVC, LinearSVC
+from sklearn.model_selection import StratifiedKFold
+from sklearn import metrics
+from matplotlib import pyplot as plt
+
+
+from helper import *
+
+import warnings
+from sklearn.exceptions import ConvergenceWarning
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
+warnings.simplefilter(action="ignore", category=ConvergenceWarning)
+
+np.random.seed(445)
+
+
+def extract_word(input_string):
+    """Preprocess review into list of tokens.
+
+    Convert input string to lowercase, replace punctuation with spaces, and split along whitespace.
+    Return the resulting array.
+
+    E.g.
+    > extract_word("I love EECS 445. It's my favorite course!")
+    > ["i", "love", "eecs", "445", "it", "s", "my", "favorite", "course"]
+
+    Input:
+        input_string: text for a single review
+    Returns:
+        a list of words, extracted and preprocessed according to the directions
+        above.
+    """
+    # TODO: Implement this function
+    for r in string.punctuation:
+        input_string = input_string.replace(r, " ")
+
+    return input_string.lower().split()
+
+def extract_dictionary(df):
+    """Map words to index.
+
+    Reads a pandas dataframe, and returns a dictionary of distinct words
+    mapping from each distinct word to its index (ordered by when it was
+    found).
+
+    E.g., with input:
+        | text                          | label | ... |
+        | It was the best of times.     |  1    | ... |
+        | It was the blurst of times.   | -1    | ... |
+
+    The output should be a dictionary of indices ordered by first occurence in
+    the entire dataset:
+        {
+           it: 0,
+           was: 1,
+           the: 2,
+           best: 3,
+           of: 4,
+           times: 5,
+           blurst: 6
+        }
+    The index should be autoincrementing, starting at 0.
+
+    Input:
+        df: dataframe/output of load_data()
+    Returns:
+        a dictionary mapping words to an index
+    """
+    word_dict = {}
+    # TODO: Implement this function
+    index = 0
+    for t in df["text"]:
+        unique_set = set(extract_word(t))
+        unique_list = list(unique_set)
+        for w in unique_list:
+            if w not in word_dict:
+                word_dict[w] = index
+                index += 1
+
+    return word_dict
+
+
+def generate_feature_matrix(df, word_dict):
+    """Create matrix of feature vectors for dataset.
+
+    Reads a dataframe and the dictionary of unique words to generate a matrix
+    of {1, 0} feature vectors for each review.  Use the word_dict to find the
+    correct index to set to 1 for each place in the feature vector. The
+    resulting feature matrix should be of dimension (# of reviews, # of words
+    in dictionary).
+
+    Input:
+        df: dataframe that has the text and labels
+        word_dict: dictionary of words mapping to indices
+    Returns:
+        a numpy matrix of dimension (# of reviews, # of words in dictionary)
+    """
+    number_of_reviews = df.shape[0]
+    number_of_words = len(word_dict)
+    feature_matrix = np.zeros((number_of_reviews, number_of_words))
+    # TODO: Implement this function
+    return feature_matrix
+
+
+def performance(y_true, y_pred, metric="accuracy"):
+    """Calculate performance metrics.
+
+    Performance metrics are evaluated on the true labels y_true versus the
+    predicted labels y_pred.
+
+    Input:
+        y_true: (n,) array containing known labels
+        y_pred: (n,) array containing predicted scores
+        metric: string specifying the performance metric (default='accuracy'
+                 other options: 'f1-score', 'auroc', 'precision', 'sensitivity',
+                 and 'specificity')
+    Returns:
+        the performance as an np.float64
+    """
+    # TODO: Implement this function
+    # This is an optional but very useful function to implement.
+    # See the sklearn.metrics documentation for pointers on how to implement
+    # the requested metrics.
+
+
+def cv_performance(clf, X, y, k=5, metric="accuracy"):
+    """Split data into k folds and run cross-validation.
+
+    Splits the data X and the labels y into k-folds and runs k-fold
+    cross-validation: for each fold i in 1...k, trains a classifier on
+    all the data except the ith fold, and tests on the ith fold.
+    Calculates and returns the k-fold cross-validation performance metric for
+    classifier clf by averaging the performance across folds.
+    Input:
+        clf: an instance of SVC()
+        X: (n,d) array of feature vectors, where n is the number of examples
+           and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+        k: an int specifying the number of folds (default=5)
+        metric: string specifying the performance metric (default='accuracy'
+             other options: 'f1-score', 'auroc', 'precision', 'sensitivity',
+             and 'specificity')
+    Returns:
+        average 'test' performance across the k folds as np.float64
+    """
+    # TODO: Implement this function
+    # HINT: You may find the StratifiedKFold from sklearn.model_selection
+    # to be useful
+
+    # Put the performance of the model on each fold in the scores array
+    scores = []
+    return np.array(scores).mean()
+
+
+def select_param_linear(
+    X, y, k=5, metric="accuracy", C_range=[], loss="hinge", penalty="l2", dual=True
+):
+    """Search for hyperparameters of linear SVM with best k-fold CV performance.
+
+    Sweeps different settings for the hyperparameter of a linear-kernel SVM,
+    calculating the k-fold CV performance for each setting on X, y.
+    Input:
+        X: (n,d) array of feature vectors, where n is the number of examples
+        and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+        k: int specifying the number of folds (default=5)
+        metric: string specifying the performance metric (default='accuracy',
+             other options: 'f1-score', 'auroc', 'precision', 'sensitivity',
+             and 'specificity')
+        C_range: an array with C values to be searched over
+        loss: string specifying the loss function used (default="hinge",
+             other option of "squared_hinge")
+        penalty: string specifying the penalty type used (default="l2",
+             other option of "l1")
+        dual: boolean specifying whether to use the dual formulation of the
+             linear SVM (set True for penalty "l2" and False for penalty "l1"ß)
+    Returns:
+        the parameter value for a linear-kernel SVM that maximizes the
+        average 5-fold CV performance.
+    """
+    # TODO: Implement this function
+    # HINT: You should be using your cv_performance function here
+    # to evaluate the performance of each SVM
+
+
+def plot_weight(X, y, penalty, C_range, loss, dual):
+    """Create a plot of the L0 norm learned by a classifier for each C in C_range.
+
+    Input:
+        X: (n,d) array of feature vectors, where n is the number of examples
+        and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+        penalty: penalty to be forwarded to the LinearSVC constructor
+        C_range: list of C values to train a classifier on
+        loss: loss function to be forwarded to the LinearSVC constructor
+        dual: whether to solve the dual or primal optimization problem, to be
+            forwarded to the LinearSVC constructor
+    Returns: None
+        Saves a plot of the L0 norms to the filesystem.
+    """
+    norm0 = []
+    # TODO: Implement this part of the function
+    # Here, for each value of c in C_range, you should
+    # append to norm0 the L0-norm of the theta vector that is learned
+    # when fitting an L2- or L1-penalty, degree=1 SVM to the data (X, y)
+
+    plt.plot(C_range, norm0)
+    plt.xscale("log")
+    plt.legend(["L0-norm"])
+    plt.xlabel("Value of C")
+    plt.ylabel("Norm of theta")
+    plt.title("Norm-" + penalty + "_penalty.png")
+    plt.savefig("Norm-" + penalty + "_penalty.png")
+    plt.close()
+
+
+def select_param_quadratic(X, y, k=5, metric="accuracy", param_range=[]):
+    """Search for hyperparameters of quadratic SVM with best k-fold CV performance.
+
+    Sweeps different settings for the hyperparameters of an quadratic-kernel SVM,
+    calculating the k-fold CV performance for each setting on X, y.
+    Input:
+        X: (n,d) array of feature vectors, where n is the number of examples
+           and d is the number of features
+        y: (n,) array of binary labels {1,-1}
+        k: an int specifying the number of folds (default=5)
+        metric: string specifying the performance metric (default='accuracy'
+                 other options: 'f1-score', 'auroc', 'precision', 'sensitivity',
+                 and 'specificity')
+        param_range: a (num_param, 2)-sized array containing the
+            parameter values to search over. The first column should
+            represent the values for C, and the second column should
+            represent the values for r. Each row of this array thus
+            represents a pair of parameters to be tried together.
+    Returns:
+        The parameter values for a quadratic-kernel SVM that maximize
+        the average 5-fold CV performance as a pair (C,r)
+    """
+    # TODO: Implement this function
+    # Hint: This will be very similar to select_param_linear, except
+    # the type of SVM model you are using will be different...
+    best_C_val, best_r_val = 0.0, 0.0
+    return best_C_val, best_r_val
+
+
+def main():
+    # Read binary data
+    # NOTE: READING IN THE DATA WILL NOT WORK UNTIL YOU HAVE FINISHED
+    #       IMPLEMENTING generate_feature_matrix AND extract_dictionary
+    X_train, Y_train, X_test, Y_test, dictionary_binary = get_split_binary_data(
+        fname="data/dataset.csv"
+    )
+    IMB_features, IMB_labels, IMB_test_features, IMB_test_labels = get_imbalanced_data(
+        dictionary_binary, fname="data/dataset.csv"
+    )
+    
+    print(extract_word("BEST book ever!"))
+    df = pd.read_csv("data/dataset.csv")
+    print(extract_dictionary(df))
+    print(len(extract_dictionary(df)))
+
+    # TODO: Questions 3, 4, 5
+
+    # Read multiclass data
+    # TODO: Question 6: Apply a classifier to heldout features, and then use
+    #       generate_challenge_labels to print the predicted labels
+
+    (multiclass_features,
+     multiclass_labels,
+     multiclass_dictionary) = get_multiclass_training_data()
+
+    heldout_features = get_heldout_reviews(multiclass_dictionary)
+
+
+if __name__ == "__main__":
+    main()
