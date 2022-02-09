35dea7207bca198f736d0639168de370e0e08663
Qifa Wang <qifwang@umich.edu>
2022-02-09 11:05:13 -0500

    Challenge

--- project1.py
+++ project1.py
@@ -8,12 +8,22 @@ import pandas as pd
 import numpy as np
 import itertools
 import string
+from sklearn import model_selection
 
 from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
 from matplotlib import pyplot as plt
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+from nltk.stem import WordNetLemmatizer
 
 
 from helper import *
@@ -523,14 +533,14 @@ def main():
     #         0.9484568192543652, 0.7995079950799509, 1.0, 0.0]
     # n10p3 = [0.8597785977859779, 0.9043624161073825, 0.9657291175082586,
     #          0.9944649446494465, 0.8292307692307692, 0.9815950920245399]
-    
+
     # plt.plot(metrics, n1p1, label="W_n=1, W_p=1")
     # plt.plot(metrics, n10p3, label="W_n=10, W_p=3")
     # plt.xlabel("Metrics")
     # plt.ylabel("Performance")
     # plt.legend()
     # plt.savefig("ROC.png")
-    
+
     # Read multiclass data
     # TODO: Question 6: Apply a classifier to heldout features, and then use
     #       generate_challenge_labels to print the predicted labels
@@ -540,15 +550,51 @@ def main():
      multiclass_dictionary) = get_multiclass_training_data()
 
     heldout_features = get_heldout_reviews(multiclass_dictionary)
-    
-    print(multiclass_features)
-    
-    df = pd.read_csv()
 
-    vectorizer = TfidfVectorizer(strip_accents="unicode")
-    X=vectorizer.fit_transform(multiclass_features)
+    print(multiclass_features)
+
+    corpus = pd.read_csv("data/dataset.csv")
+
+    corpus["text"] = [t.lower() for t in corpus["text"]]
+
+    corpus["text"] = [word_tokenize(t) for t in corpus["text"]]
+    tag_map = defaultdict(lambda: wn.NOUN)
+    tag_map['J'] = wn.ADJ
+    tag_map['V'] = wn.VERB
+    tag_map['R'] = wn.ADV
+
+    for index, t in enumerate(corpus["text"]):
+        Final_words = []
+        word_lemmatized = WordNetLemmatizer()
+
+        for word, tag in pos_tag(t):
+            if word not in stopwords.words('english') and word.isalpha():
+                word_final = word_lemmatized.lemmatize(word, tag_map[tag[0]])
+                Final_words.append(word_final)
+        corpus.loc[index, "text_final"] = str(Final_words)
+
+    X, x, Y, y = model_selection.train_test_split(
+        corpus["text_final"], corpus["label"])
+
+    Encoder = LabelEncoder()
+
+    Y = Encoder.fit_transform(Y)
+    y = Encoder.fit_transform(y)
+
+    vectorizer = TfidfVectorizer().fit(corpus["text_final"])
+
+    train = vectorizer.transform(X)
+    test = vectorizer.transform(x)
+
+    clf = LinearSVC(C=1, penalty="l2", loss="hinge", dual=True)
+    clf.fit(train, Y)
+
+    prediction = clf.predict(test)
+
+    print(performance(y, prediction, metric="accuracy"))
 
     print()
+
 
 if __name__ == "__main__":
     main()
